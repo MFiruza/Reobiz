cf21a6351c7e544fb209a53bda47a9874e29054c
MFiruza <firuzaa@code.edu.az>
2021-12-04 02:16:04 +0400

    changed 4

--- js/nav.js
+++ js/nav.js
@@ -1,23 +1,35 @@
-"use strict"
-$(document).ready(function(){
-    $('#side-button').click(function() {
-        $('.side-info').css('right','-522px');
+$(document).ready(function () {
+    $('#side-button').click(function () {
+        $('.side-info').css('right', '-522px');
     });
 
-    $('#grafics').click(function() {
-        $('.side-info').css('right','0');
+    $('#grafics').click(function () {
+        $('.side-info').css('right', '0');
+    });
+    //responsive
+
+   
+    $('.sub-op-btn').click(function () {
+        $('.sub-op-btn>span>i').toggleClass("down");
+        $('.sub-section').toggleClass("d-block");
+    });
+        
+    $('.close-btn').click(function () {
+        $('.sub-op-btn>span>i').toggleClass("down");
+        $('.sub-section').toggleClass("d-block");
+    });
+///////////////////////////////////////////////////////
+    $('.sub-op-btn2').click(function () {
+        $('.sub-op-btn2>span>i').toggleClass("down");
+        $('.sub-section2').toggleClass("d-block");
+    });
+        
+    $('.close-btn2').click(function () {
+        $('.sub-op-btn2>span>i').toggleClass("down");
+        $('.sub-section2').toggleClass("d-block");
+    });
+    $('.nav-bars').click(function () {
+        $('.fixed-nav').toggleClass("d-block");
     });
 
-
-    $('.nav-bars>a').click(function(ev){
-        // do whatever you want here
-    
-        ev.preventDefault();
-        ev.stopPropagation();
-    });
-
-
-
-
-
-  });
+});
