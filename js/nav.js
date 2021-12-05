f983867c07e73c416eb0b41035be7727a845d7c8
MFiruza <firuzaa@code.edu.az>
2021-12-06 03:57:03 +0400

    changed 12-06-2021

--- js/nav.js
+++ js/nav.js
@@ -8,22 +8,22 @@ $(document).ready(function () {
     });
     //responsive
 
-   
+
     $('.sub-op-btn').click(function () {
         $('.sub-op-btn>span>i').toggleClass("down");
         $('.sub-section').toggleClass("d-block");
     });
-        
+
     $('.close-btn').click(function () {
         $('.sub-op-btn>span>i').toggleClass("down");
         $('.sub-section').toggleClass("d-block");
     });
-///////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////
     $('.sub-op-btn2').click(function () {
         $('.sub-op-btn2>span>i').toggleClass("down");
         $('.sub-section2').toggleClass("d-block");
     });
-        
+
     $('.close-btn2').click(function () {
         $('.sub-op-btn2>span>i').toggleClass("down");
         $('.sub-section2').toggleClass("d-block");
@@ -31,5 +31,15 @@ $(document).ready(function () {
     $('.nav-bars').click(function () {
         $('.fixed-nav').toggleClass("d-block");
     });
-
+    /////up arrow
+    $(window).scroll(function () {
+        if ($(this).scrollTop() >= 350) {
+            $('#up-arrow').fadeIn(200);
+        } else {
+            $('#up-arrow').fadeOut(200);
+        }
+    });
+    $('#up-arrow').on('click', function () {
+        $("html, body").animate({ scrollTop: 0 }, 1000);
+    });
 });
---
+++ js/slider.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+    
+    $('.responsive').slick({
+        dots: true,
+        infinite: false,
+        speed: 1500,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+
+        autoplay: true,
+        infinite: true,
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+                breakpoint: 1186,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+
+            },
+            {
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 575,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+    $('.one-time').slick({
+        dots: true,
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 1,
+        adaptiveHeight: true
+      });
+});
